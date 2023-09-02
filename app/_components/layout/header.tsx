b3c6b8d3523e8a95dd7c604c1619f301eff00375
Mark Vu <markvu.work@gmail.com>
2023-09-02 22:37:07 +0700

    Add spinner title bar effect

--- app/_components/layout/header.tsx
+++ app/_components/layout/header.tsx
@@ -4,7 +4,6 @@ import { routes } from "@/app/_constants";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
 import { RotateButton } from "..";
 
 export function Header() {
---
+++ app/_components/layout/spinner-title-bar-effect.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+
+const getLoadingCharacter = (number: number) => {
+  switch (number % 4) {
+    case 3:
+      return "/";
+    case 2:
+      return "-";
+    case 1:
+      return "\\";
+    case 0:
+      return "|";
+    default:
+      return "";
+  }
+};
+
+export function SpinnerTitleBarEffect() {
+  useEffect(() => {
+    let counter = 0;
+    const intervalId = setInterval(() => {
+      document.title = `${getLoadingCharacter(counter)} Mark Vu`;
+      counter++;
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  return <></>;
+}
