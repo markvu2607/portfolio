a195d477508c6baa72c292bfc51114e3176c939e
Mark Vu <markvu.work@gmail.com>
2023-09-07 23:28:29 +0700

    Create contact

---
+++ app/_components/home/contact-section.tsx
@@ -0,0 +1,33 @@
+import { SectionWrapper } from "@/_components";
+import Image from "next/image";
+import Link from "next/link";
+
+export function ContactSection() {
+  return (
+    <SectionWrapper title="contact" viewMore={{ visible: false }}>
+      <div className="flex gap-4">
+        <span className="flex-1 text-gray">
+          I’m interested in freelance opportunities. However, if you have other
+          request or question, don’t hesitate to contact me.
+        </span>
+        <div className="flex-1 flex flex-col gap-4 p-4 border border-gray">
+          <span className="font-semibold">Message me here</span>
+          <div>
+            <Link href={process.env.GITHUB || "/"} target="_blank">
+              <div className="flex items-center gap-1">
+                <Image src="/github.svg" alt="Github" width={32} height={32} />
+                <span className="text-gray">markvu2607</span>
+              </div>
+            </Link>
+            <Link href={`mailto:${process.env.EMAIL}`}>
+              <div className="flex items-center gap-1">
+                <Image src="/email.svg" alt="Email" width={32} height={32} />
+                <span className="text-gray">{process.env.EMAIL}</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </SectionWrapper>
+  );
+}
