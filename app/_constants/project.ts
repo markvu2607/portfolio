5b6a47f3adae3d3c91b71957a5c66e0e20400cbc
Mark Vu <markvu.work@gmail.com>
2023-09-09 12:41:45 +0700

    [Feature] Create about me page (#40)

--- app/_constants/project.ts
+++ app/_constants/project.ts
@@ -23,3 +23,24 @@ export const projects: TProject[] = [
     liveLink: "https://portfolio-markvu2607.vercel.app/",
   },
 ];
+
+export const smallProjects: TProject[] = [
+  {
+    name: "Portfolio",
+    description: "Mark Vu portfolio",
+    technologies: ["NextJs", "Framer-Motion", "TailwindCSS"],
+    liveLink: "https://portfolio-markvu2607.vercel.app/",
+  },
+  {
+    name: "Portfolio",
+    description: "Mark Vu portfolio",
+    technologies: ["NextJs", "Framer-Motion", "TailwindCSS"],
+    liveLink: "https://portfolio-markvu2607.vercel.app/",
+  },
+  {
+    name: "Portfolio",
+    description: "Mark Vu portfolio.",
+    technologies: ["NextJs", "Framer-Motion", "TailwindCSS"],
+    liveLink: "https://portfolio-markvu2607.vercel.app/",
+  },
+]
--- app/_types/project.ts
+++ app/_types/project.ts
@@ -1,7 +1,7 @@
 export type TProject = {
   name: string;
   description: string;
-  thumbnail: string;
   technologies: string[];
   liveLink: string;
+  thumbnail?: string;
 }
