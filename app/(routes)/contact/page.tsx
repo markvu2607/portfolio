ef6684131147abb905b1838b21a8ce5fb39cbcdf
Mark Vu <markvu.work@gmail.com>
2023-09-10 22:14:17 +0700

    [Bugfix] Update account in env (#41)

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -15,10 +15,16 @@ export default function Contact() {
         <div className="flex-1 flex flex-col gap-4 p-4 border border-gray">
           <span className="font-semibold">Message me here</span>
           <div>
-            <Link href={process.env.GITHUB || "/"} target="_blank">
+            <Link
+              href={
+                `${process.env.GITHUB_PROVIDER}${process.env.GITHUB_ACCOUNT}` ||
+                "/"
+              }
+              target="_blank"
+            >
               <div className="flex items-center gap-1">
                 <Image src="/github.svg" alt="Github" width={32} height={32} />
-                <span className="text-gray">markvu2607</span>
+                <span className="text-gray">{process.env.GITHUB_ACCOUNT}</span>
               </div>
             </Link>
             <Link href={`mailto:${process.env.EMAIL}`}>
@@ -37,10 +43,16 @@ export default function Contact() {
         viewMore={{ visible: false }}
       >
         <div className="flex gap-4">
-          <Link href={process.env.GITHUB || "/"} target="_blank">
+          <Link
+            href={
+              `${process.env.GITHUB_PROVIDER}${process.env.GITHUB_ACCOUNT}` ||
+              "/"
+            }
+            target="_blank"
+          >
             <div className="flex items-center gap-1">
               <Image src="/github.svg" alt="Github" width={32} height={32} />
-              <span className="text-gray">markvu2607</span>
+              <span className="text-gray">{process.env.GITHUB_ACCOUNT}</span>
             </div>
           </Link>
           <Link href={`mailto:${process.env.EMAIL}`}>
