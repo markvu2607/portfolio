08845ccda2e83c9e090aef4ee4ffa106396515e4
Mark Vu <markvu.work@gmail.com>
2024-08-08 21:56:40 +0700

    ✨ feat: add notion blog link

--- src/feature/shared/constants.ts
+++ src/feature/shared/constants.ts
@@ -1,6 +1,10 @@
 export const routes = [
   { name: "home", link: "/" },
-  // { name: "blog", link: "/blog" },
+  {
+    name: "blog",
+    link: "https://sequoia-sort-212.notion.site/Mark-s-Blog-2fc8bc71caaf40f5ae441d650c977218",
+    target: "_blank",
+  },
   { name: "projects", link: "/projects" },
   { name: "about-me", link: "/about-me" },
   { name: "contact", link: "/contact" },
